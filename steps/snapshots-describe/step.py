04f402b930867904462b8cccd514994741bc8b03
Geoff Woodburn <geoff@woodburndesigns.com>
2020-11-16 10:32:29 -0800

    Adding ownerIds and a default of self

--- steps/snapshots-describe/step.py
+++ steps/snapshots-describe/step.py
@@ -31,9 +31,14 @@ sess = boto3.Session(
   region_name=relay.get(D.aws.region),
   aws_session_token=session_token
 )
-  
+
+owner_ids = relay.get(D.ownerIDs)
+
+if (len(owner_ids) == 0):
+  owner_ids = "self"
+
 ec2 = sess.resource('ec2')
-raw_snapshots = ec2.snapshots.all()
+raw_snapshots = ec2.snapshots.filter(OwnerIds=[owner_ids])
 
 snapshot_list = [snapshot for snapshot in raw_snapshots]
 if (len(snapshot_list) == 0):
@@ -44,7 +49,7 @@ print('Found the following EC2 snapshots:\n')
 print("{:<30} {:<30} {:<30}".format('ID', 'STATE', 'VOLUME ID'))
 for snapshot in raw_snapshots:
   print("{:<30} {:<30} {:<30}".format(snapshot.snapshot_id, snapshot.state, snapshot.volume_id))
-snapshots = list(map(partial(snapshot_to_dict, ec2), ec2.snapshots.all()))
+snapshots = list(map(partial(snapshot_to_dict, ec2), ec2.snapshots.filter(OwnerIds=[owner_ids])))
 
 print('\nAdding {0} snapshot(s) to the output `snapshots`'.format(len(snapshots)))
 relay.outputs.set('snapshots', snapshots)
